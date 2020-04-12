514806d017182c80e41b1cf085a2ab9c9251ed6a
Eduardo Sarmento <edusrmt@ufrn.edu.br>
2020-04-12 20:38:40 -0300

    Final version

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# confusion
-Restaurant website developed with React for educational purpose.
+# Ristorante Con Fusion
+
+This website was created through the course "Front-End Web Development with React" by The Hong Kong University of Science and Technology on Coursera.
+
+## Usage
+
+1. Install json-server globally by typing the following at the command prompt:
+` npm install json-server -g`
+2. Inside the json-server folder, run the following at the command prompt to start the server:
+`json-server --watch db.json -p 3001 -d 2000`
+3. Inside the frontend folder, run the following at the command prompt to start the React application:
+`npm start`
---
+++ json-server/public/precache-manifest.ae84e37f1826ff92a0b10e8f98146581.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e43182dc945d867f8659855b0bbf283",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c33e007212eb5251e2db",
+    "url": "/static/css/2.c5d64b3a.chunk.css"
+  },
+  {
+    "revision": "4d35674509a75212aa46",
+    "url": "/static/css/main.d5b512bc.chunk.css"
+  },
+  {
+    "revision": "c33e007212eb5251e2db",
+    "url": "/static/js/2.7a70f5a5.chunk.js"
+  },
+  {
+    "revision": "e6ff647c2a6af9cdfc810fe3a8fc8680",
+    "url": "/static/js/2.7a70f5a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d35674509a75212aa46",
+    "url": "/static/js/main.e8daece8.chunk.js"
+  },
+  {
+    "revision": "7356d298505eec143204",
+    "url": "/static/js/runtime-main.06f63e2e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
